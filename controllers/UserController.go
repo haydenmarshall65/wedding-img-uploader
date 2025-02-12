1ed35ee041888049291950030fd9373ff9c33380
haydenmarshall65 <hmarshall@altorntl.com>
2025-02-12 16:02:47 -0600

    working on the User controller

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"errors"
 	"hayden/wedding-img-uploader/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
+	"gorm.io/gorm"
 )
 
 func CreateUser(c *gin.Context) {
@@ -25,11 +28,30 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUsers(c *gin.Context) {
+	var users []models.User
+	models.DB.Find(&users)
 
+	c.IndentedJSON(200, users)
 }
 
 func GetUser(c *gin.Context) {
+	idStr := c.Param("id")
 
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.IndentedJSON(400, struct{ message string }{message: "Invalid User search."})
+		return
+	}
+
+	var user models.User
+	user.ID = id
+
+	if err := models.DB.First(&user).Error; err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+		c.IndentedJSON(404, struct{ message string }{message: "User not found."})
+		return
+	}
+
+	c.IndentedJSON(200, user)
 }
 
 func UpdateUser(c *gin.Context) {
